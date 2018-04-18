0d585d7dc2e2cc912d4e3f526505b75e4b83b64d
Askcore <171witb14@o365.htl-leoben.at>
2018-04-18 09:34:01 +0200

    Erstes Python Programm

---
+++ hello-world.py
@@ -0,0 +1,10 @@
+
+print ("Hallo, Ich bin ein Computer namens AlexbertaCorones")
+name = input("Wie ist dein Name? ")
+
+print ("Schön dich zu treffen ", name ,". Dein Name ist ", len(name) ," Zeichen lang.", sep="")
+
+alter = input ("Wie alt bist du? ")
+alter = int(alter)
+
+print ("Du wirst in einem Jahr ", alter+1 ," Jahre alt!", sep="")
