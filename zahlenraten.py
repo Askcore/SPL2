f8a0148538c7e8d0d1c14f1811ca783ea5d48bcc
Askcore <171witb14@o365.htl-leoben.at>
2018-04-18 12:26:57 +0200

    Finished!

--- zahlenraten.py
+++ zahlenraten.py
@@ -6,8 +6,13 @@ zufallszahl = random.randint(1, z2)
 
 fertig = False
 versuche = 0
+zEingabe = 0
 
-while(fertig == false)
-    zEingabe = input("Geben sie eine Zahl zwischen 1 und", z2 ,"ein:")
+while(fertig == False):
+    print("Geben sie eine Zahl zwischen 1 und", z2 ,"ein:", end="")
+    zEingabe = input()
+    zEingabe = int(zEingabe)
     versuche +=1
-    if(zEingabe)
+    if(zEingabe == zufallszahl):
+        fertig = True
+        print("Du hast", versuche ,"Versuche gebraucht.")
